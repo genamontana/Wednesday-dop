dee8cf607fac632886cf576cee916665bc3921ef
genamontana <predator199@yandex.ru>
2023-04-08 22:19:04 +0300

    2 dop in process 3 mission complete

--- extra_classes_wednesday_2.3/src/task_3/Task_3.tsx
+++ extra_classes_wednesday_2.3/src/task_3/Task_3.tsx
@@ -1,19 +1,25 @@
 import {ChangeEvent, useState} from 'react';
-import { SlowComponent } from './slowComponent/SlowComponent';
+import {SlowComponent} from './slowComponent/SlowComponent';
 
 
 //find the problem and fix it as part of composition optimization, memo, children
 
 export const Task_3 = () => {
-  const [value, setValue] = useState('');
 
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => setValue(event.currentTarget.value);
-
-  return (
-    <div>
-      <div>Lags when change value</div>
-      <input type="text" value={value} onChange={onChange} />
-      <SlowComponent />
-    </div>
-  );
+    return (
+        <div>
+            <div>Lags when change value</div>
+            <Input/>
+            <SlowComponent/>
+        </div>
+    );
 };
+
+export const Input = () => {
+
+    const [value, setValue] = useState('');
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => setValue(event.currentTarget.value);
+
+    return <input type="text" value={value} onChange={onChange}/>
+}
